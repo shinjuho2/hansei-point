545aa8c5c36768cfd133fca1f0016ad92a7af6fc
shinjuho2 <sinjuho2@gmail.com>
2024-01-04 22:06:31 +0900

    Login

--- src/routes/loginPage/index.js
+++ src/routes/loginPage/index.js
@@ -7,13 +7,15 @@ import ContextRouter from "../../contextRouter";
 import ProductListPage from "../productListPage";
 import { FaArrowLeft } from 'react-icons/fa';
 import PlainButton from "../../components/plainButton";
+import { ApiLogin } from "../../api";
+import ContextStore from "../../contextStore";
 
 /// 로그인페이지
 const LoginPage = () => {
     const { pushAndUtilRemoved, push, pop } = useContext(ContextRouter);
-    const [id, setId] = useState('');
-    const [passwd, setPasswd] = useState('');
-
+    const { storeLoginData } = useContext(ContextStore);
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [userPw, setUserPw] = useState('');
     return (<div className={style.LoginPage}>
         <div className={style.LoginBox}>
             <div className={style.LoginBoxText}>
@@ -23,17 +25,48 @@ const LoginPage = () => {
                 <h3 className={style.LoginText1}>hansei</h3>
                 <h3 className={style.LoginText2}>point</h3>
             </div>
-            <OutlineInput className={style.LoginInput} placeholder='휴대전화번호(- 제외)' />
-            <OutlineInput className={style.LoginInput} type='password' placeholder='패스워드' />
+            <OutlineInput
+                value={phoneNumber}
+                className={style.LoginInput}
+                placeholder='휴대전화번호(- 제외)'
+                onInput={(value) => setPhoneNumber(value.replace(/[^0-9]/g, '').slice(0, 11))}
+            />
+            <OutlineInput
+                value={userPw}
+                className={userPw.LoginInput}
+                type='password'
+                placeholder='패스워드'
+                onInput={setUserPw}
+            />
             <p className={style.LoginDescription}>
                 아직 회원이 아니신가요?{' '}
                 <a onClick={() => push(JoinPage.name)}>
                     회원가입
                 </a>
             </p>
-            <OutlineButton onClick={() => {
-                //id.length || passwd.length < 11 ? window.confirm("휴대전화 및 패스워드를 입력해주세요.") : null
-                pushAndUtilRemoved(ProductListPage.name)
+            <OutlineButton onClick={async () => {
+                if (phoneNumber.length === 0 || userPw === 0) {
+                    alert('휴대전화 및 패스워드를 입력해주세여');
+                    return;
+                }
+
+                if (phoneNumber.length !== 11) {
+                    alert('휴대전화가 올바르지 않습니다');
+                    return;
+                }
+
+                if (userPw.length < 6) {
+                    alert('패스워드가 올바르지 않습니다');
+                    return;
+                }
+
+                const resp = await ApiLogin(phoneNumber, userPw);
+                if (resp.code === '0000') {
+                    storeLoginData(resp.data);
+                    pushAndUtilRemoved(ProductListPage.name)
+                } else {
+                    alert('로그인에 실패하였습니다');
+                }
             }}>
                 로그인
             </OutlineButton>
--- src/routes/rewardPointPage/index.js
+++ src/routes/rewardPointPage/index.js
@@ -15,7 +15,6 @@ const RewardPointPage = () => {
         const value = e.target.value;
         setPhoneNumber(prev => {
             const phoneString = (prev.replace(/[^0-9]/g, '') + value).slice(0, 11);
-            console.log(phoneString);
             return phoneString.replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, '$1-$2-$3').replace(/\-{1,2}$/g, "");
         });
     };
