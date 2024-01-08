49a83f979c4f21f3af194402ddc634880b20a83e
shinjuho2 <sinjuho2@gmail.com>
2024-01-08 22:39:19 +0900

    app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import ContextRouter from './contextRouter';
-import { useState, useMemo, useCallback } from 'react';
+import { useState, useMemo, useCallback, useEffect, useLayoutEffect } from 'react';
 
 import MainPage from './routes/mainPage';
 import JoinPage from './routes/joinPage';
@@ -10,7 +10,8 @@ import RewardPointPage from './routes/rewardPointPage';
 
 import './Components.css';
 import RewardPointCompletePage from './routes/rewardPointCompletePage';
-import ContextStore from './contextStore';
+import ContextStore, { storeLoginDataKey } from './contextStore';
+import { ApiCurrentPoint } from './api';
 
 function App() {
   const [prevRouteNames, setPrevRouteNames] = useState([]);
@@ -34,17 +35,34 @@ function App() {
     setRouteName(name);
   }, [routeName]);
 
+  useLayoutEffect(() => {
+    const loginData = window.sessionStorage.getItem(storeLoginDataKey);
+    if (loginData) {
+      setLoginData(JSON.parse(loginData));
+      setRouteName(ProductListPage.name);
+    }
+}, []);
+
   return (
     <ContextStore.Provider value={{
       loginData,
       storeLoginData(loginData) {
+        window.sessionStorage.setItem(storeLoginDataKey, JSON.stringify(loginData));
         setLoginData(loginData);
       },
       removeLoginData() {
+        window.sessionStorage.removeItem(storeLoginDataKey);
         setLoginData(null);
       },
-      updatePointData() {
-        /// 직접구현
+      async updatePointData() {
+        if (!loginData) return;
+        const resp = await ApiCurrentPoint(loginData.userId);
+        if (resp.code === '0000') {
+          setLoginData(prev => ({
+            ...prev,
+            point: resp.data,
+          }));
+        }
       },
     }}>
       <ContextRouter.Provider value={{
